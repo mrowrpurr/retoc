fe6bbfc2c717afa0458829e8f7a954753aded6db
Mrowr Purr <mrowr.purr@gmail.com>
2025-05-02 22:25:27 -0700

    C++ placeholder

---
+++ cpp/pak_utoc_ucas/xmake.lua
@@ -0,0 +1,17 @@
+target("pak_utoc_ucas")
+    set_kind("binary")
+    add_files("src/*.cpp")
+    add_includedirs("include", { public = true })
+    
+    -- Add dependencies on any other C++ static libraries in this folder:
+    add_deps("oodle_loader")
+    add_deps("pak", "utoc", "ucas")
+    
+    -- Add dependencies on any packages:
+    --
+    -- Example:
+    -- add_packages("spdlog")
+    --
+    -- See xmake.lua in the root folder for a list of available packages.
+    --
+    -- If you want a package which isn't available, please just stop and ask :)
